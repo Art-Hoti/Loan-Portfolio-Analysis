f30bcbb493b0b28660d2d1902ab0ac8caeb88e2f
Art Hoti <mrhoti@Arts-MacBook-Pro.local>
2024-07-26 14:59:46 +0200

    Describe the changes in db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -45,3 +45,32 @@ class RDSDatabaseConnector:
         if self.connection:
             self.connection.close()
             print("Disconnected from the database.")
+import yaml
+
+def load_credentials(filepath):
+    with open(filepath, 'r') as file:
+        credentials = yaml.safe_load(file)
+    return credentials
+from sqlalchemy import create_engine
+import pandas as pd
+
+class RDSDatabaseConnector:
+    def __init__(self, credentials):
+        self.host = credentials['RDS_HOST']
+        self.password = credentials['RDS_PASSWORD']
+        self.user = credentials['RDS_USER']
+        self.database = credentials['RDS_DATABASE']
+        self.port = credentials['RDS_PORT']
+        self.engine = None
+    def init_engine(self):
+        self.engine = create_engine(
+            f'postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}'
+        )
+    def extract_data(self):
+        if self.engine is None:
+            raise Exception("Engine not initialized. Call init_engine() first.")
+        query = "SELECT * FROM loan_payments"
+        df = pd.read_sql(query, self.engine)
+        return df
+    def save_data_to_csv(self, df, filename):
+        df.to_csv(filename, index=False)
